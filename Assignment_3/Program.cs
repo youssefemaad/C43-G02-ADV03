3fa69b53941b53d95829b5009dd34176c79571e2
youssefemaad <eyoussef228@gmail.com>
2025-02-01 20:57:57 +0200

    new

--- Assignment_3/Program.cs
+++ Assignment_3/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace AdvancedCSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Assignment 03 - Advanced C#");
+
+            #region Question 1 - Implement fPtr
+            /*
+            Considering the Code Below, Write Down the Body of all Listed Methods and Properties,
+            you need to Define fPtr as the following cases:
+            
+            a. User Defined Delegate Datatype
+            b. BCL Delegates
+            c. Anonymous Method (GetISBN)
+            d. Lambda Expression (GetPublicationDate)
+            */
+            #endregion
+
+            #region Book Class
+            // Define the Book class here
+            #endregion
+
+            #region BookFunctions Class
+            // Define the BookFunctions class here
+            #endregion
+
+            #region LibraryEngine Class
+            // Define the LibraryEngine class here
+            #endregion
+        }
+    }
+}
